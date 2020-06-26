eaae1235f3a931de527bbb4cedb665f109e5607d
Luis Felipe Alipio <luis.souza@parnamirim.rn.gov.br>
2020-06-26 12:39:48 -0300

    Setores

--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -5,11 +5,11 @@ import { PortalRoutingModule } from './portal-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortalComponent } from './portal/portal.component';
-import { InicialSectionComponent } from './components/inicial-section/inicial-section.component';
 
+import { DepartmentsComponent } from './components/departments/departments.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, PortalComponent, InicialSectionComponent],
+  declarations: [HeaderComponent, FooterComponent, PortalComponent, DepartmentsComponent],
   imports: [
     CommonModule,
     PortalRoutingModule
